fe3160ffd821c9bf0d3931afe7384e5fbcbe245b
Helen Lv <213660742@qq.com>
2026-06-08 19:58:30 +0800

    1 commit

---
+++ FirstRepo.java
@@ -0,0 +1,18 @@
+/*
+Class Hello
+*/
+
+public class FirstRepo {
+    public static void main(String[] arg){
+        int i=3;
+System.out.println("FirstRepo World，Rebecca");
+i++;
+System.out.println("FirstRepo World, Jason");
+i++;
+System.out.println("FirstRepo World, H");
+i++;
+System.out.println("FirstRepo World, Hello");
+    }
+
+   
+}
